ba095c29c236552b3249e21f8b065d31a273675d
Jesús Rodríguez <D-rider@users.noreply.github.com>
2015-03-23 20:01:16 +0100

    Update HCSR04_Six_Sensors.cpp
    Still not tested with arduino board

--- Drone-Control-Software/HCSR04-Ultrasound-Library/HCSR04_Six_Sensors/HCSR04_Six_Sensors.cpp
+++ Drone-Control-Software/HCSR04-Ultrasound-Library/HCSR04_Six_Sensors/HCSR04_Six_Sensors.cpp
@@ -3,13 +3,13 @@
 **  Ultrasound modules.                                                **
 **                                                                     **
 **  Created by Jesus Rodriguez Venzal, IEEE Student Branch of Granada  **
-**  03/Mar/2015                                                        **
+**  17/Mar/2015                                                        **
 ************************************************************************/
 
 #include "Arduino.h"
-#include "HCSR04_Multiple_Sensors.h"
+#include "HCSR04_Six_Sensors.h"
 
-HCSR04::HCSR04(int trig_in[],int echo_in[],int num_sensors){
+HCSR04::HCSR04(int trig_in[],int echo_in[]){
 /**********************************************************************
 **  When defining an object of this class it will storage in private **
 **  variables triger and echo pins vectors, defining each triger pin **
@@ -17,22 +17,22 @@ HCSR04::HCSR04(int trig_in[],int echo_in[],int num_sensors){
 **  state.                                                           **
 **********************************************************************/
 
-  _number_sensors = num_sensors;  //Load number of sensors used
-  triger = new int[_number_sensors];
-  echo = new int[_number_sensors];  /*Create triger and echo arrays with the length of the number of sensors
+  _number_sensors = 6;  //Load number of sensors used
+  _triger = new int[_number_sensors];
+  _echo = new int[_number_sensors];  /*Create triger and echo arrays with the length of the number of sensors
   and load the address of the first element in the pointer*/
 
   for (int i = 0; i < _number_sensors; i++){
-    triger[i] = trig_in[i];  //Load triger pins
-    pinMode(triger[i], OUTPUT);  //Define them as OUTPUT
-    digitalWrite(triger[i], LOW);  //Set them as LOW to avoid problems
-    echo[i] = echo_in[i];  //Load echo pins
-    pinMode(echo[i], INPUT);  //Define them as INPUT
-    digitalWrite(echo[i], LOW);  //Set them as LOW to avoid problems
+    _triger[i] = trig_in[i];  //Load triger pins
+    pinMode(_triger[i], OUTPUT);  //Define them as OUTPUT
+    digitalWrite(_triger[i], LOW);  //Set them as LOW to avoid problems
+    _echo[i] = echo_in[i];  //Load echo pins
+    pinMode(_echo[i], INPUT);  //Define them as INPUT
+    digitalWrite(_echo[i], LOW);  //Set them as LOW to avoid problems
   }
 }
 
-void HCSR04::Reading(int distancia[]){
+void HCSR04::Reading(){
 /************************************************************************
 **  This function send an ultrasonic pulse with every sensor reading   **
 **  the duration of the echo received. After a triger 10 us HIGH       **
@@ -45,18 +45,24 @@ void HCSR04::Reading(int distancia[]){
 
   //Set all the sensors as LOW during 2 us to stabilize them
   for (int i = 0; i < _number_sensors; i++){
-    digitalWrite(triger[i], LOW);
+    digitalWrite(_triger[i], LOW);
   }
   delayMicroseconds(2);
   //Send 10 us start ranging signal
   for (int i = 0; i < _number_sensors; i++){
-    digitalWrite(triger[i], HIGH);
+    digitalWrite(_triger[i], HIGH);
   }
   delayMicroseconds(10);
   for (int i = 0; i < _number_sensors; i++){
-    digitalWrite(triger[i], LOW);
-    distancia[i] = pulseIn(echo[i], HIGH) /29 / 2 ;  //Distance in cm (1/29 = 0.034 cm/us)
+    digitalWrite(_triger[i], LOW);
+    _distance[i] = pulseIn(_echo[i], HIGH) /29 / 2 ;  //Distance in cm (1/29 = 0.034 cm/us)
   }
+  TOP = _distance[0];
+  BOTTOM = _distance[1];
+  FRONT = _distance[2];
+  REAR = _distance[3];
+  LEFT = _distance[4];
+  RIGHT = _distance[5];
 }
 
 void HCSR04::Test(int trig_outside[], int echo_outside[]){
@@ -67,12 +73,12 @@ void HCSR04::Test(int trig_outside[], int echo_outside[]){
 
   Serial.println("Testing allocation of triger and echo pins:");
   for (int i = 0; i < _number_sensors; i++){
-    if (trig_outside[i] != triger[i]){
+    if (trig_outside[i] != _triger[i]){
       Serial.print("Error in triger allocation: Triger pin ");
       Serial.print(trig_outside[i]);
       Serial.println(" is corrupt inside the class");
     }
-    else if (echo_outside[i] != echo[i]){
+    else if (echo_outside[i] != _echo[i]){
       Serial.print("Error in echo allocation: Echo pin ");
       Serial.print(echo_outside[i]);
       Serial.println(" is corrupt inside the class");
