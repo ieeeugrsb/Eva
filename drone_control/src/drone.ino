d112874443d45cd9754c2e91cb90b7f93125497c
Benito Palacios Sánchez <benito356@gmail.com>
2015-09-28 13:52:03 +0200

    Add the IMU libs and show info

--- drone_control/src/drone.ino
+++ drone_control/src/drone.ino
@@ -13,7 +13,21 @@
   along with this file.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_9DOF.h>
+
 #include "Motor.h"
+
+// Assign a unique ID to the sensors.
+Adafruit_9DOF                 dof   = Adafruit_9DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+// Update this with the correct SLP for accurate altitude measurements
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
 
 // Create motors instance and start in Working-Mode.
 Motor motor_bl(12); // Back-Left Motor
@@ -24,6 +38,33 @@ Motor motor_fr( 9); // Front-Right Motor
 int value = 0;
 int incremento = 5;
 
+void initSensors()
+{
+  if(!accel.begin()) {
+    // There was a problem detecting the LSM303 ... check your connections
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+
+  if(!mag.begin()) {
+    // There was a problem detecting the LSM303 ... check your connections
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+void setup()
+{
+    // Setup PC communication.
+    Serial.begin(9600);
+
+    // Init sensors
+    initSensors();
+
+    // Wait for motors going to working mode.
+    delay(2000);
+}
+
 void write2motors(int value)
 {
     Serial.print("Writing "); Serial.println(value);
@@ -33,22 +74,49 @@ void write2motors(int value)
     motor_fr.write(value);
 }
 
-void setup()
-{
-    // Setup PC communication.
-    Serial.begin(9600);
-
-    // Wait for motors going to working mode.
-    delay(2000);
-}
-
-void loop()
+void moveMotors()
 {
     if (value > 25 || value < 0)
         incremento *= -1;
 
     value += incremento;
     write2motors(value);
+}
 
+void showSensorData()
+{
+    sensors_event_t accel_event;
+    sensors_event_t mag_event;
+    sensors_vec_t   orientation;
+
+    // Calculate pitch and roll from the raw accelerometer data.
+    accel.getEvent(&accel_event);
+    if (dof.accelGetOrientation(&accel_event, &orientation)) {
+      // 'orientation' should have valid .roll and .pitch fields
+      Serial.print("Roll: ");
+      Serial.print(orientation.roll);
+      Serial.print("; ");
+      Serial.print("Pitch: ");
+      Serial.print(orientation.pitch);
+      Serial.print("; ");
+    }
+
+    // Calculate the heading using the magnetometer
+    mag.getEvent(&mag_event);
+    if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)) {
+      // 'orientation' should have valid .heading data now
+      Serial.print("Heading: ");
+      Serial.print(orientation.heading);
+      Serial.print("; ");
+    }
+
+    Serial.println("");
+    delay(1000);
+}
+
+void loop()
+{
+    showSensorData();
+    moveMotors();
     delay(2000);
 }
